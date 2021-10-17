bec42439e7013e920394cbb7354a4f157cda5b61
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-17 11:41:26 -0400

    PhysicsTraveller

--- PortalDemo/Assets/Scripts/Demo/PortalPhysicsObject.cs
+++ PortalDemo/Assets/Scripts/Demo/PortalPhysicsObject.cs
@@ -10,4 +10,16 @@ public class PortalPhysicsObject : PortalTraveller {
     public Color[] colors;
     static int i;
 
+    void Awake () {
+        rigidbody = GetComponent<Rigidbody> ();
+        graphicsObject.GetComponent<MeshRenderer> ().material.color = colors[i];
+        i++;
+        if (i > colors.Length - 1) { i = 0; }
+    }
+
+    public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
+        base.Teleport (fromPortal, toPortal, pos, rot);
+        rigidbody.velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (rigidbody.velocity));
+        rigidbody.angularVelocity = toPortal.TransformVector (fromPortal.InverseTransformVector (rigidbody.angularVelocity));
+    }
 }
